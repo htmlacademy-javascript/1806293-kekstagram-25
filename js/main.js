d6f24e3e8d002e6792149707d957f95ca78ef16f
Dmitry <efos.ds@gmail.com>
2022-02-26 16:47:35 +0700

    поменял первую функцию на стрелочную

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 // Задача 1
 
 // источник https://learn.javascript.ru/task/random-int-min-max
-function isRandomInteger(min, max) {
-  const RANDOM_COUNT = min + Math.random() * (max + 1 - min);
-  return Math.floor(RANDOM_COUNT);
-}
-isRandomInteger();
+
+const getRandomInteger = (min, max) => 
+  min + Math.floor(Math.random() * (max + 1 - min));
+
+getRandomInteger();
+
 
 // Задача 2
-const isCheckLineLength = (checkString, maxLength) => (checkString?.length <= maxLength) ? true : false;
-isCheckLineLength();
+const checkLineLength = (checkString, maxLength) => checkString?.length <= maxLength;
 
-// У меня все время вылазила ошибка "Uncaught TypeError: Cannot read properties of undefined (reading 'length')"
-// Коллега посоветовал добавить "?" для проверки на null, такое допустимо?
+checkLineLength();
